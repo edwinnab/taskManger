21fea6e62e384fe119fbb8318b4b1cde38add573
edwinnab <edwinnabikeri@gmail.com>
2022-05-02 16:15:07 +0300

    taskManger Application

---
+++ src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {TaskService} from "../../task.service";
+
+@Component({
+  selector: 'app-task-view',
+  templateUrl: './task-view.component.html',
+  styleUrls: ['./task-view.component.scss']
+})
+export class TaskViewComponent implements OnInit {
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+  }
+
+  createNewList() {
+    this.taskService.createList('Testing').subscribe((response: any) => {
+      console.log(response)
+    })
+
+  }
+
+}
