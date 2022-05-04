f9bac73922eba0405fcb093ca5843d5526b635b2
edwinnab <edwinnabikeri@gmail.com>
2022-05-04 16:36:32 +0300

    changes on cors

--- taskApis/app.js
+++ taskApis/app.js
@@ -11,6 +11,21 @@ const { List, Task } = require('./db/models')
 
 app.use(bodyParser.json());
 
+//CORS MIDDLEWARE FOR EXPRESS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
+
+    res.header(
+        'Access-Control-Expose-Headers',
+        'x-access-token, x-refresh-token'
+    );
+
+    next();
+});
+
+
 /*
 list routes
 
@@ -111,7 +126,7 @@ app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
     }, {
         $set: req.body
     }).then(() => {
-        res.sendStatus(200);
+        res.send({message: 'Updated Successfully!!'});
     })
 })
 
